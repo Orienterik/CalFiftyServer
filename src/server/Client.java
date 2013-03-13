daefba2a8d53c0127d05d2ceaaa7fed596795712
Erik Gustafsson <erik@himle.st>
2013-03-13 15:29:51 +0100

    Nye klasser

---
+++ src/server/Client.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+
+public class Client {
+	
+	BufferedReader bfr;
+	
+	Client() {
+		
+	}
+
+}
---
+++ src/server/Server.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.util.ArrayList;
+
+public class Server {
+	
+	private Database db;
+	private ArrayList<Client> clients;
+
+}
