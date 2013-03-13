97fcabd585aeb25ca91fc14ba6d637e0afc41eaf
Erik Gustafsson <erik@himle.st>
2013-03-13 15:57:52 +0100

    Oppdatert Server.java og Database.java

--- src/server/Database.java
+++ src/server/Database.java
@@ -1,5 +1,9 @@
 package server;
 
 public class Database {
+	
+	Database() {
+		
+	}
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -6,5 +6,20 @@ public class Server {
 	
 	private Database db;
 	private ArrayList<Client> clients;
+	
+	public static void main(String[] args) {
+		Server server = new Server();
+		while (true) {
+			server.WaitForConnections();
+		}
+	}
+	
+	Server() {
+		db = new Database();
+	}
+	
+	void WaitForConnections() {
+		
+	}
 
 }
